cb72ebf24808f6f95e5d6d07311b02e5afdfa755
Ameer <ameerulislam@gmail.com>
2022-08-06 10:10:33 +0600

    WIP 6 min

--- ooppyrefresher/Tutorial 4: Inheritance - Creating Subclasses.py
+++ ooppyrefresher/Tutorial 4: Inheritance - Creating Subclasses.py
@@ -1,6 +1,6 @@
 class Employee:
 
-    raise_amount = 1.04
+    raise_amt = 1.04
     num_of_emps = 0
 
     def __init__(self, first, last, pay): 
@@ -13,5 +13,24 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount) 
+        self.pay = int(self.pay * self.raise_amt) 
     
+class Developer(Employee):
+    raise_amt = 1.10
+
+
+dev_1 = Developer('Ameer', 'Ul Islam', 50000)
+dev_1_1 = Employee('Ameer', 'Ul Islam', 50000)
+dev_2 = Developer('Kazol', 'Akter', 60000)
+
+print(dev_1.email)
+print(dev_2.email)
+
+# print(help(Developer)) # this will print out all info about the Developer class. Where it was inherited from methods etc
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
+
+print(dev_1_1.pay)
+dev_1_1.apply_raise()
+print(dev_1_1.pay)
